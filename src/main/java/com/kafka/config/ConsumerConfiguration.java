8b374b942346a95ce9bb103a6fe6d5ad847edc8d
Emmanuel <theemanuel211@gmail.com>
2024-12-29 12:55:00 -0600

    Creating the Kafka Consumer

---
+++ src/main/java/com/kafka/config/ConsumerConfiguration.java
@@ -0,0 +1,23 @@
+package com.kafka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class ConsumerConfiguration {
+
+    @Bean
+    ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+        return objectMapper;
+    }
+}
